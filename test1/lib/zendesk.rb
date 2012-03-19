5eb52524c89d4940356ae7315dd54186d79d072f
Juris Galang <jurisgalang@gmail.com>
2012-03-18 20:16:18 -0700

    fixed implemenation and use for #responder; added .gitignore

--- test1/lib/zendesk.rb
+++ test1/lib/zendesk.rb
@@ -17,40 +17,40 @@ module Zendesk
     def open_ticket description, options = { }
       options.assert_valid_keys(:ticket_type_id, :priority_id, :requester_email, :requester_name)
       payload = { ticket: options.merge(description: description) }
-      resource("/tickets", with_payload: true).post(payload, &responder)
+      resource("/tickets", with_payload: true).post(payload, &method(:responder))
     end
     
     def get_ticket id
-      ticket = resource("/tickets/#{id}").get(&responder)
+      ticket = resource("/tickets/#{id}").get(&method(:responder))
       ticket["ticket"] unless ticket.nil?
     end
   
     def close_ticket id, options = { }
       options.assert_valid_keys(:assignee_id, :status_id)
       payload = { ticket: { status_id: 4 }.merge(options) }
-      resource("/tickets/#{id}", with_payload: true).put(payload, &responder)
+      resource("/tickets/#{id}", with_payload: true).put(payload, &method(:responder))
     end
 
     def delete_ticket id
-      resource("/tickets/#{id}").delete(&responder)
+      resource("/tickets/#{id}").delete(&method(:responder))
     end
     
     def create_user email, name, role_id, restriction_id
       payload = { user: { email: email, name: name, roles: role_id, restriction_id: restriction_id } }
-      resource("/users", with_payload: true).post(payload, &responder)
+      resource("/users", with_payload: true).post(payload, &method(:responder))
     end
    
     def delete_user id
-      resource("/users/#{id}").delete(&responder)
+      resource("/users/#{id}").delete(&method(:responder))
     end
     
     def get_user id
-      user = resource("/users/#{id}").get(&responder)
+      user = resource("/users/#{id}").get(&method(:responder))
       user["user"] unless user.nil?
     end
      
     def get_users
-      resource("/users").get(&responder)["users"]
+      resource("/users").get(&method(:responder))["users"]
     end
 
     private
@@ -61,16 +61,14 @@ module Zendesk
       RestClient::Resource.new(url, options)
     end
     
-    def responder 
-      ->(response, request, result, &block) {
-        case response.code
-        when 200 then (response.strip.empty? ? true : Crack::XML.parse(response))
-        when 201 then result['location'].split("/").last.to_i
-        when 302 then response.follow_redirection(request, result, &block)
-        when 404 then nil
-        else raise RequestError, response
-        end
-      }
+    def responder response, request, result, &block
+      case response.code
+      when 200 then (response.strip.empty? ? true : Crack::XML.parse(response))
+      when 201 then result['location'].split("/").last.to_i
+      when 302 then response.follow_redirection(request, result, &block)
+      when 404 then nil
+      else raise RequestError, response
+      end
     end
   end
 end
